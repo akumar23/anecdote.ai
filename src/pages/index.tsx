bbe6aae413199babdffb4a58c5a73a5cb9f472c5
akumar23 <aryan.kumar@nalej.com>
2024-03-25 18:40:33 -0700

    increased font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000000] to-[#1c1d2d]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[15rem]">
             Anecdote <span className="text-[hsl(280,100%,70%)]">AI</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
